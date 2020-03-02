67f0ddd607b395f20dc9c34b28a49b191adee861
Ivan Rao <ivan.rao@hotmail.com>
2020-03-02 18:56:09 -0300

    Cambios

--- atm/src/hardwareDevices/CashDispenser.java
+++ atm/src/hardwareDevices/CashDispenser.java
@@ -2,18 +2,14 @@ package hardwareDevices;
 
 
 public class CashDispenser{
-	private double _totalCash = 10000;
+	private double totalCash = 10000;
 
 	public CashDispenser() {
 	};
 
-	public double getTotalCash() {
-		return _totalCash;
-	}
-
 	public boolean withdrawCash(double balance, double ammount) {
-		if (ammount <= balance && ammount <= _totalCash) {
-			_totalCash -= ammount;
+		if (ammount <= balance && ammount <= totalCash) {
+			totalCash -= ammount;
 			return true;
 		} else {
 			return false;
